f4c4e820952a3db048914a77e54c19a1fb2f13ad
thomasbrettell <70102377+thomasbrettell@users.noreply.github.com>
2021-07-17 19:06:52 +1000

    Begin development of jquery based survey framework

--- src/index.js
+++ src/index.js
@@ -1,55 +1,302 @@
 import $ from 'jquery'
-import Backbone from 'backbone'
-import _ from 'underscore'
+require("jquery-html5-validity")($);
 
-const rootID = '#app-root';
+const rootElement = $('#survey');
+const submissionID = Math.floor(Math.random(0, 1000)*100)
+let questionQueue = []
+let questionCount = 0
+let officialQuestionCount = 0
 
-let songsArray = [
-  {title: 'Hiasd'},
-  {title: 'cxiqq'},
-  {title: 'czxk'}
-]
+const fakeSurveyJSON = {
+  id: '1',
+  title: 'Fake Survey',
+  questions: [
+    {
+      title: 'Textbox',
+      type: 'text',
+      elements: [
+        {
+          type: 'text'
+        }
+      ]
+    },
+    {
+      id: 'dsad',
+      title: 'How much wood',
+      type: 'other',
+      elements: [
+        {
+          title: 'other_1',
+          type: 'other',
+        }
+      ]
+    },
+    {
+      id: 'blaaah',
+      title: 'Is this?',
+      type: 'radiogroup',
+      elements: [
+        {
+          title: 'other_1',
+          type: 'radiogroup',
+          choices: [
+            '1111',
+            '2222',
+            '3333',
+            '4444',
+            'Other'
+          ]
+        }
+      ],
+      otherQuestion: {
+        id: 'blaaah_other',
+        title: 'Is this other?',
+        type: 'other',
+        elements: [
+          {
+            title: 'other_1',
+            type: 'other'
+          }
+        ]
+      }
+    }
+  ]
+}
 
+generateQuestion(fakeSurveyJSON.questions[officialQuestionCount])
 
-var SongModel = Backbone.Model.extend()
+function generateElements(question) {
+  let elements = question.elements
+  let form = question.rootElement.find('form')
 
-var SongsCollection = Backbone.Collection.extend({
-  model: SongModel
-})
+  elements.forEach(function(element, index) {
+    question.rootElement.find('form').append(`
+      <fieldset name='question_${index}'>
+        <legend>${element.title}</legend>
+      </fieldset>
+    `)
+    let fieldset = question.rootElement.find('fieldset').eq(index)
 
-var SongView = Backbone.View.extend({
+    switch(element.type) {
+      case 'checkbox':
+        element.choices.forEach(function(choice) {
+          fieldset.append(`
+            <label class='checkbox'>
+              ${choice}
+              <input type='checkbox' value='${choice}' name='question_${index}' required this>
+            </label>
+          `)
+          $('[this]').change(function() {
+            checkFormValidity(form)
+          })
+          $('[this]').removeAttr('this')
+        })
+        break;
 
-  tagName: 'h1',
+      case 'text':
+        fieldset.append(`
+          <label class='text'>
+            <span>${element.title}</span>
+            <input type='text' name='question_${index}' required this>
+          </label>
+        `)
+        $('[this]').keyup(function() {
+          checkFormValidity(form)
+        })
+        $('[this]').keyup(function() {
+          console.log('!')
+        })
+        $('[this]').removeAttr('this')
+        break;
 
-  render: function() {
-    console.log(this)
+      case 'radiogroup':
+        element.choices.forEach(function(choice) {
+          fieldset.append(`
+            <label class='radiogroup'>
+              ${choice}
+              <input type='radio' value='${choice}' name='question_${index}' required this>
+            </label>
+          `)
+          $('[this]').change(function() {
+            checkFormValidity(form)
+          })
+          $('[this]').removeAttr('this')
+        })
+        break;
 
-    this.$el.html(this.model.get('title'))
+      case 'other':
+        fieldset.append(`
+          <label class='text'>
+            <input type='text' name='other_1' required this>
+          </label>
+          <button type='button'>Add</button>
+        `)
+        $('[this]').keyup(function() {
+          checkFormValidity(form)
+        })
+        $('[this]').removeAttr('this')
+        fieldset.find('button').click(function() {
+          let self = $(this)
+          fieldset.find('label').last().after(`
+            <label class='text'>
+              <input type='text' name='other_${index}' required this>
+            </label>
+          `)
+          $('[this]').keyup(function() {
+            checkFormValidity(form)
+          })
+          $('[this]').removeAttr('this')
+          if(fieldset.find('input').length === 3) {
+            self.remove()
+          }
+        })
+        break;
 
-    return this
-  }
-})
+      case 'rating':
+          fieldset.append(`
+            <label class='rating'>
+              1
+              <input type='radio' value='1' name='${element.title}' required this>
+            </label>
+            <label class='rating'>
+              2
+              <input type='radio' value='2' name='${element.title}' required this>
+            </label>
+            <label class='rating'>
+              3
+              <input type='radio' value='3' name='${element.title}' required this>
+            </label>
+            <label class='rating'>
+              4
+              <input type='radio' value='4' name='${element.title}' required this>
+            </label>
+            <label class='rating'>
+              5
+              <input type='radio' value='5' name='${element.title}' required this>
+            </label>
+          `)
+          $('[this]').change(function() {
+            checkFormValidity(form)
+          })
+          $('[this]').removeAttr('this')
+        break;
+    }
+  })
 
-var SongsView = Backbone.View.extend({
-  render: function() {
-    var self = this
+  form.append(`
+    <button type='submit' disabled this>submit</button>
+  `)
+  $('[this]').click(function(e) {
+    e.preventDefault()
+    processQuestion(question, form)
+  })
+  $('[this]').removeAttr('this')
+}
 
-    this.model.each(function(SongModel) {
-      var songView = new SongView({model:SongModel})
-      self.$el.append(songView.render().$el)
+function generateQuestion(question) {
+  rootElement.html('')
+  rootElement.append(`
+    <div id='question-root-${question.id}'>
+      <form>
+        <h3>${question.title}</h3>   
+      </form>
+    </div>
+  `)
+  question.rootElement = $(`#question-root-${question.id}`)
+
+  generateElements(question)
+}
+
+function checkFormValidity(form) {
+  let target = form.find('fieldset').length
+  let validFields = 0;
+
+  form.find('fieldset').each(function() {
+    $(this).find('input').each(function() {
+      if($(this).isValid()) {
+        validFields++
+        return false
+      }
     })
+  })
+
+  if(validFields === target) {
+    form.find('button[type="submit"]').removeAttr('disabled')
+    return true
+  } else {
+    form.find('button[type="submit"]').attr('disabled', true)
+    return false
   }
-})
+}
 
-var songsCollection = new SongsCollection([])
+function processQuestion(question, form) {
 
-_.each(songsArray, function(song, index){
-  console.log('!')
-  songsCollection.add(new SongModel({title: song.title, id: index}))
-})
+  if (checkFormValidity(form)) {
+    officialQuestionCount++
+    let response = {
+      id: question.id,
+      response: formatResponse(form.serializeArray(), question.type),
+      submission_id: submissionID
+    }
+    console.log(response)
 
-console.log(songsCollection)
+    if(question.otherQuestion) {
+      if(checkOther(response.response)) {
+        questionQueue.push(question.otherQuestion)
+      }
+    }
+  
+    if(questionQueue.length > 0) {
+      generateQuestion(questionQueue[0])
+      questionQueue.shift()
+    } else {
+      generateQuestion(fakeSurveyJSON.questions[officialQuestionCount])
+    }
+  }
+}
 
-$(`${rootID}`).append('<ul></ul>')
-var songsView = new SongsView({el: `${rootID} ul`, model: songsCollection})
-songsView.render()
+function formatResponse(response, type) {
+  let responses = []
+
+  switch(type) {
+    case 'checkbox': 
+      response.forEach(function(input) {
+        responses.push(input.value)
+      })
+      return responses
+
+    case 'radiogroup':
+      return response[0].value
+
+    case 'text':
+      return response[0].value
+
+    case 'other':
+      response.forEach(function(input) {
+        responses.push(input.value)
+      })
+      return responses
+
+    case 'rating':
+      return response[0].value
+
+    case 'multirating':
+      console.log(response)
+      response.forEach(function(input) {
+        responses.push({
+          keys: input.name,
+          value: input.value
+        })
+      })
+      return responses
+  }
+}
+
+function checkOther(response) {
+  console.log(response)
+  if(response.includes('Other')) {
+    return true
+  } else {
+    return false
+  }
+}
