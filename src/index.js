16dddb6b45b98e0a5d8b99d44b12ee226c92381a
thomasbrettell <70102377+thomasbrettell@users.noreply.github.com>
2021-07-28 22:51:24 +1000

    React app test

--- src/index.js
+++ src/index.js
@@ -1,56 +1,17 @@
-import * as ScrollMagic from "scrollmagic";
-import 'scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators'
-import 'scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap'
-import gsap from 'gsap'
-import $ from 'jquery'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-console.log($)
-console.log(ScrollMagic)
-console.log(gsap)
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-const dotPath = {
-  curviness: 1.25,
-  values: [
-    {x: window.innerWidth, y: 0}
-  ]
-}
-
-const oneTween = new TimelineLite()
-
-oneTween.add(
-  TweenLite.to('.one .dot', 1, {
-    bezier: dotPath,
-    ease: Power1.easeInOut
-  })
-)
-
-const twoTween = new TimelineLite()
-
-twoTween.add(
-  TweenLite.to('.two .dot', 1, {
-    bezier: dotPath,
-  })
-)
-
-
-const controller = new ScrollMagic.Controller()
-
-const scene = new ScrollMagic.Scene({
-  triggerElement: '.one',
-  duration: 3000,
-  triggerHook: .25
-})
-.setTween(oneTween)
-.addIndicators()
-.setPin('.one')
-.addTo(controller)
-
-const sceneTwo = new ScrollMagic.Scene({
-  triggerElement: '.two',
-  duration: 3000,
-  triggerHook: .25
-})
-.setTween(twoTween)
-.addIndicators()
-.setPin('.two')
-.addTo(controller)
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
