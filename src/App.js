246d2ccdfbf68136bdb54dee9bb913dcff9e9640
thomasbrettell <70102377+thomasbrettell@users.noreply.github.com>
2021-08-04 20:18:44 +1000

    Foood Appp

--- src/App.js
+++ src/App.js
@@ -1,41 +1,24 @@
-import React, { useState, useEffect } from 'react';
-
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
-import AuthContext from './context/auth-context';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  useEffect(() => {
-    const loggedInStatus = localStorage.getItem('isLoggedIn')
-
-    if (loggedInStatus === 'true') {
-      setIsLoggedIn(true)
-    }
-  }, [isLoggedIn])
-
-  const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
-    localStorage.setItem('isLoggedIn', true)
-    setIsLoggedIn(true);
-  };
-
-  const logoutHandler = () => {
-    setIsLoggedIn(false);
-    localStorage.removeItem('isLoggedIn')
-  };
-
   return (
-    <AuthContext.Provider value={{isLoggedIn: isLoggedIn, onLogout: logoutHandler, onLogin: loginHandler}}>
-      <MainHeader />
-      <main>
-        {!isLoggedIn && <Login />}
-        {isLoggedIn && <Home />}
-      </main>
-    </ AuthContext.Provider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Foood Appp
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
